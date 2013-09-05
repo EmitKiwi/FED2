892c023f51dc7db6e1a55965d7ddf89653e0ed3d
Lars Hisken <lars@codespartans.com>
2013-09-05 12:09:39 +0200

    Testing merge

--- les1/cmdaan.js
+++ les1/cmdaan.js
@@ -90,6 +90,68 @@ function _calculate_distance(p1, p2){
 }
 
 
+var map = {
+	generate_map: function(myOptions, canvasId) {
+		// TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
+		debug_message("Genereer een Google Maps kaart en toon deze in #"+canvasId)
+		map = new google.maps.Map(document.getElementById(canvasId), myOptions);
+
+		var routeList = [];
+		// Voeg de markers toe aan de map afhankelijk van het tourtype
+		debug_message("Locaties intekenen, tourtype is: "+tourType);
+		for (var i = 0; i < locaties.length; i++) {
+
+			// Met kudos aan Tomas Harkema, probeer local storage, als het bestaat, voeg de locaties toe
+			try {
+				(localStorage.visited==undefined||isNumber(localStorage.visited))?localStorage[locaties[i][0]]=false:null;
+			} catch (error) {
+				debug_message("Localstorage kan niet aangesproken worden: "+error);
+			}
+
+		    var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
+		  	routeList.push(markerLatLng);
+
+		    markerRij[i] = {};
+		    for (var attr in locatieMarker) {
+	            markerRij[i][attr] = locatieMarker[attr];
+	        }
+		    markerRij[i].scale = locaties[i][2]/3;
+
+		    var marker = new google.maps.Marker({
+		        position: markerLatLng,
+		        map: map,
+		        icon: markerRij[i],
+		        title: locaties[i][0]
+		    });
+		}
+	// TODO: Kleur aanpassen op het huidige punt van de tour
+		if(tourType == LINEAIR){
+			// Trek lijnen tussen de punten
+			debug_message("Route intekenen");
+			var route = new google.maps.Polyline({
+		  		clickable: false,
+		  		map: map,
+		  		path: routeList,
+		  		strokeColor: 'Black',
+		  		strokeOpacity: .6,
+		  		strokeWeight: 3
+		  	});
+
+		}
+
+		// Voeg de locatie van de persoon door
+		currentPositionMarker = new google.maps.Marker({
+	        position: kaartOpties.center,
+	        map: map,
+	        icon: positieMarker,
+	        title: 'U bevindt zich hier'
+	    });
+
+		// Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
+		ET.addListener(POSITION_UPDATED, update_positie);
+	}
+}
+
 // GOOGLE MAPS FUNCTIES
 /**
  * generate_map(myOptions, canvasId)
@@ -103,65 +165,6 @@ function _calculate_distance(p1, p2){
  *	@param canvasID:string - het id van het HTML element waar de
  *		kaart in ge-rendered moet worden, <div> of <canvas>
  */
-function generate_map(myOptions, canvasId){
-// TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
-	debug_message("Genereer een Google Maps kaart en toon deze in #"+canvasId)
-	map = new google.maps.Map(document.getElementById(canvasId), myOptions);
-
-	var routeList = [];
-	// Voeg de markers toe aan de map afhankelijk van het tourtype
-	debug_message("Locaties intekenen, tourtype is: "+tourType);
-	for (var i = 0; i < locaties.length; i++) {
-
-		// Met kudos aan Tomas Harkema, probeer local storage, als het bestaat, voeg de locaties toe
-		try {
-			(localStorage.visited==undefined||isNumber(localStorage.visited))?localStorage[locaties[i][0]]=false:null;
-		} catch (error) {
-			debug_message("Localstorage kan niet aangesproken worden: "+error);
-		}
-
-	    var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
-	  	routeList.push(markerLatLng);
-
-	    markerRij[i] = {};
-	    for (var attr in locatieMarker) {
-            markerRij[i][attr] = locatieMarker[attr];
-        }
-	    markerRij[i].scale = locaties[i][2]/3;
-
-	    var marker = new google.maps.Marker({
-	        position: markerLatLng,
-	        map: map,
-	        icon: markerRij[i],
-	        title: locaties[i][0]
-	    });
-	}
-// TODO: Kleur aanpassen op het huidige punt van de tour
-	if(tourType == LINEAIR){
-		// Trek lijnen tussen de punten
-		debug_message("Route intekenen");
-		var route = new google.maps.Polyline({
-	  		clickable: false,
-	  		map: map,
-	  		path: routeList,
-	  		strokeColor: 'Black',
-	  		strokeOpacity: .6,
-	  		strokeWeight: 3
-	  	});
-
-	}
-
-	// Voeg de locatie van de persoon door
-	currentPositionMarker = new google.maps.Marker({
-        position: kaartOpties.center,
-        map: map,
-        icon: positieMarker,
-        title: 'U bevindt zich hier'
-    });
-
-	// Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
-	ET.addListener(POSITION_UPDATED, update_positie);
-}
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
